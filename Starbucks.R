094d1cf6aa90623fdf3a3dea22902ccf41112980
carneybill <bill@rstudio.com>
2017-10-10 16:06:44 -0400

    inital

---
+++ Starbucks.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(leaflet)
+
+file <- "https://opendata.socrata.com/api/views/ddym-zvjk/rows.csv"
+starbucks <- read_csv(file)
+
+nyc <- starbucks %>%
+  filter(City == "New York") %>%
+  select(Brand, Name, Latitude, Longitude)
+
+leaflet() %>% addTiles() %>% setView(-73.983, 40.760, zoom = 12) %>% 
+  addMarkers(data = nyc, lat = ~ Latitude, lng = ~ Longitude, icon = , popup = nyc$Name)
+
+head(nyc)
